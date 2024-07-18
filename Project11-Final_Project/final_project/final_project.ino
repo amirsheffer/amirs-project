f67f9388d39a5d5a476a817da6361310cde5129d
Amir Sheffer <amir.sheffer@weizmann.ac.il>
2024-07-18 13:34:09 +0300

    fnal_working_example_until_stage_4

--- Project11-Final_Project/final_project/final_project.ino
+++ Project11-Final_Project/final_project/final_project.ino
@@ -5,13 +5,15 @@
 #include <Servo.h>
 
 // Servo variables
-Servo myservo;  // Create servo object to control a servo
-int pos = 0;    // Variable to store the servo position
-int fanPin = 7; // Set D7 as control switch 
+Servo myservo;  
+int pos = 0;    
+int fanPin = 7; 
 int fanState = LOW;
-const float g = 9.8; // Acceleration due to gravity in m/s^2
+int BuzzerState = LOW;
+int BuzzerPin = 5;
+
 // Accelerometer setup
-LIS3DHTR<TwoWire> LIS; // IIC
+LIS3DHTR<TwoWire> LIS; 
 #define WIRE Wire
 
 U8X8_SSD1306_128X64_NONAME_HW_I2C u8x8(U8X8_PIN_NONE);
@@ -29,49 +31,50 @@ void setup() {
   LIS.setOutputDataRate(LIS3DHTR_DATARATE_50HZ);
   
   // Servo setup
-  myservo.attach(3);  // Attaches the servo on pin 3 to the servo object
+  myservo.attach(3); // Attach servo to pin 3
   pinMode(fanPin, OUTPUT);
-}
 
+// buzer setup
+  pinMode(BuzzerPin, OUTPUT);
+
+}
 void loop() {
   // Makes fan rotate
   fanState = HIGH;
   digitalWrite(fanPin, fanState);
 
-  // Move servo from 0 to 100 degrees
-  for (pos = 0; pos <= 100; pos += 1) {
-    myservo.write(pos);              // Tell servo to go to position in variable 'pos'
-    delay(40);                       // Waits 40ms for the servo to reach the position
-  }
-
-  // Move servo from 100 to 0 degrees
-  for (pos = 100; pos >= 0; pos -= 1) {
-    myservo.write(pos);              // Tell servo to go to position in variable 'pos'
-    delay(40);                       // Waits 40ms for the servo to reach the position
-  }
-
-   float zAcceleration = LIS.getAccelerationZ();
+  // Read Z-axis acceleration
+  float zAcceleration = LIS.getAccelerationZ();
+  
+  // Map Z-axis acceleration from [-1, 1] to [180, 0]
+  float angle = 180 - ((zAcceleration + 1) / 2 * 180); 
 
   
-  // Calculate elevation angle
-  float angle = 180 - ((zAcceleration + 1) / 2) * 180; 
 
-  // float angle = atan(zAcceleration / g) * (180.0 / PI); // Convert to degrees
+  // Check angle for buzzer 
+  if (angle > 100) {
+    tone(BuzzerPin, 250); // Turn buzzer ON
+  } else {
+    noTone(BuzzerPin); // Turn buzzer OFF
+  }
+
+// Set servo position to the calculated angle
+  myservo.write(angle); 
 
   // Display information
-  u8x8.setFont(u8x8_font_chroma48medium8_r);
+  u8x8.setFont(u8x8_font_chroma48medium8_r); // Use smaller font
   
-  if (!LIS) {
-    u8x8.setCursor(0, 0);
-    u8x8.print("LIS3DHTR didn't connect.");
-    while (1);
-  }
+  // if (!LIS) {
+  //   u8x8.setCursor(0, 0);
+  //   u8x8.print("LIS3DHTR didn't connect.");
+  //   while (1);
+  // }
 
   u8x8.setCursor(0, 0);
   u8x8.print("Elevation angle:");  
-  u8x8.setCursor(0, 2);
+  u8x8.setCursor(0, 1);
   u8x8.print(angle);
   u8x8.print(" degrees");
 
-  delay(100); // Delay before the next update
+  delay(100); // Smaller delay before the next update
 }
